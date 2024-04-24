7daf850e86ce06c7bf999ce8962dc522aeb34a90
746837Lucio <luciosalazar0503@gmail.com>
2024-04-24 15:50:30 -0700

    added sopme scripts
    added assdciprtsd
    '

--- SalazarP3PersonalProject/Assets/scripts/PlayerController.cs
+++ SalazarP3PersonalProject/Assets/scripts/PlayerController.cs
@@ -16,13 +16,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); 
-        float verticalInput = Input.GetAxis("Vertical");
-
-
-        playerRb.AddForce(Vector3.forward * speed * verticalInput);
-        playerRb.AddForce(Vector3.right * speed * horizontalInput);
-        if (transform.position.z <-zBound)
-
+        MovePlayer();
+        ConstrainPlayerPosition();
     }
+
+
+    void MovePlayer()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+
+            playerRb.AddForce(Vector3.forward * speed * verticalInput);
+            playerRb.AddForce(Vector3.right * speed * horizontalInput);
+        }
+        
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+
+        
+
+    
 }
